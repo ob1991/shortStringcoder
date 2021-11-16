ec44968478b1660c8ffeaf98f3476090c71757bb
80336854 <zhaoqiang12@oppo.com>
2021-11-16 10:01:22 +0800

    add .gitignore

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	var shortStringCoder = NewShortStringCoder([]byte("1234567890abcdef"))
 	str := "123456781234"
-	encodeStr := shortStringCoder.Encode(&str)
+	encodeStr := shortStringCoder.Encode([]byte(str))
 	decodeStr := shortStringCoder.Decode(encodeStr)
 	fmt.Printf("%v,orin %v", string(decodeStr), str)
 }
